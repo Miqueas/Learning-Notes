7d4a8e1b3a33b6b58d51860cb740c130f4de5607
Miqueas Martinez <miqueas2020@yahoo.com>
2021-09-26 09:57:48 -0500

    Done containers.nim

---
+++ containers.nim
@@ -0,0 +1,56 @@
+#[ Containers ]#
+
+# Arrays: homogeneous and fixed length containers, indexing starts at 0
+#         and the size must be known at compile time
+var
+  nums1: array[5, int] = [ 2, 4, 6, 8, 10 ]
+  # This is valid too
+  nums2 = [ 1, 3, 5, 7, 9 ]
+  strlist = [ "hello", "world" ]
+
+echo nums1
+echo nums2
+echo strlist
+
+# Sequences: like arrays, but the size don't need to be known at compile
+#            time
+var
+  nums3: seq[int]
+  seqliteral = @[ "abc", "def", "ghi" ]
+  # Anothe way to init an empty sequence
+  myname = newSeq[string]()
+
+myname.add("Miqueas")
+myname.add("Martinez")
+
+for i in 0 .. 20:
+  if i mod 2 == 0:
+    nums3.add(i)
+
+echo nums3
+echo seqliteral
+echo myname
+
+# Tuples: like arrays (fixed size), but data is heterogeneous and
+#         uses key-value pairs (optional)
+var
+  cat = ("Bingus", true)
+  me = (
+    name: "Miqueas",
+    is_nerd: true,
+    likes: [ "coding", "anime", "music" ]
+  )
+
+echo cat
+echo me
+
+#[ Indexing and slicing containers ]#
+
+# Indexing starts from 0, use ^1 to reverse counting (^1 is the
+# last element)
+echo nums1[^1]
+echo seqliteral[2]
+
+# Slicing works with the for syntax: start .. stop or start ..< stop
+echo nums3[3 .. 7]
+echo nums3[5 ..< 10]
