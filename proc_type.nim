87639a50f9860de5a5178e27ba7a52c3282a72a1
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-10 09:46:53 -0500

    Added procedural types

---
+++ proc_type.nim
@@ -0,0 +1,16 @@
+#[
+  Passing functions to another functions in Nim is really simple
+]#
+proc greet(name: string): string =
+  "Hello, " & name & "!"
+
+proc bye(name: string): string =
+  "Goodbye, " & name & "."
+
+proc talk(fn: proc(n: string): string, name = "guest"): void =
+  echo fn(name)
+
+talk(greet, "Pablo")
+talk(bye, "Maria")
+talk(greet)
+talk(bye)
