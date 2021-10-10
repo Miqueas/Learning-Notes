a5d52b44b01c99559040179dea103ec3d64d5bf3
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-10 09:46:38 -0500

    Added references and pointers

---
+++ ref_ptr.nim
@@ -0,0 +1,28 @@
+#[
+  Nim has support for references and pointer. The differences between
+  these are simple: references are traced by the garbage collector,
+  but pointers are untraced. Any reference/pointer that points to
+  nothing has the 'nil' value. To create a reference/pointer, use the
+  'ref'/'ptr' keyword. Pointers are unsafe and references are safe
+]#
+type
+  IntRef = ref int
+  IntPtr = ptr int
+
+var
+  i: int = 7
+  ir: IntRef = new(int)
+  # use cast[T]() instead of T() to prevent an error
+  ip: IntPtr = cast[IntPtr](alloc(sizeof(int)))
+
+# [] is to de-reference, ye is a weird syntax
+ir[] = i * i
+ip[] = i * i * i
+
+echo i
+echo ir[]
+echo ip[]
+
+if ip != nil:
+  # pointers needs to be manually de-allocated
+  dealloc(ip)
