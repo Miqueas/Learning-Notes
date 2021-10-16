642dfbbad524d55fc5941c15277bb0b9dc36c866
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-15 21:50:11 -0500

    Added include statement

---
+++ include.nim
@@ -0,0 +1,8 @@
+#[
+  There's nothing too complex about the include statement, it's
+  literally like in C: just include another file content inside
+  the current file
+]#
+include procedures1
+
+echo 29.fib()
