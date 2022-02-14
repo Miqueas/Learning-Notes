16a7153d29f7ed18035e8739f19d6207cf1c6377
Miqueas <miqueas2020@yahoo.com>
2022-02-14 15:06:35 +0000

    Updated hello

--- hello.dart
+++ hello.dart
@@ -1,6 +1,13 @@
 // An small overview of Dart
+int fib(int n) {
+  if (n < 2) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
 void main(List<String> args) {
   print("E");
+  print("fib(10) = ${fib(10)}");
 
   if (args.isNotEmpty) {
     print("Arguments:");
