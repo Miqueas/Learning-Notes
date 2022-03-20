084f82272ab81a31e94076d87854bc4b686d9047
Miqueas <miqueas2020@yahoo.com>
2022-03-20 13:58:28 +0000

    Nim: minor changes in exceptions

--- Nim/exceptions.nim
+++ Nim/exceptions.nim
@@ -4,11 +4,13 @@
 
 # The {.raises.} pragma can be used to tell the compiler
 # that a function can raise an error
-proc greet(name: string = "guest") {.raises: [ValueError].} =
+proc greet(name = "guest") {.raises: [ValueError].} =
   if name == "":
     let e = new(ValueError)
     e.msg = "`name` is empty"
     # Only `ref object` can be raised
+    # This can be simplified to:
+    #   newException(ValueError, "`name` is empty")
     raise e
 
   echo "Hello, " & name & "!"
