628afd02809f2b1f69cc4f52081fe713fd1142ba
Miqueas <miqueas2020@yahoo.com>
2022-03-20 14:00:54 +0000

    Nim: added `{.inline.}` pragma in proc type

--- Nim/proc_type.nim
+++ Nim/proc_type.nim
@@ -1,13 +1,13 @@
 #[
   Passing functions to another functions in Nim is really simple
 ]#
-proc greet(name: string): string =
+proc greet(name: string): string {.inline.} =
   "Hello, " & name & "!"
 
-proc bye(name: string): string =
+proc bye(name: string): string {.inline.} =
   "Goodbye, " & name & "."
 
-proc talk(fn: proc (n: string): string; name = "guest"): void =
+proc talk(fn: proc (n: string): string {.inline.}, name = "guest") =
   echo fn(name)
 
 talk(greet, "Pablo")
