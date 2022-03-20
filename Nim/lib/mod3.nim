a7ef6c294c1a9267effe8ad74f1fe4d8dd377357
Miqueas <miqueas2020@yahoo.com>
2022-03-20 14:07:30 +0000

    Nim: changed `ref object` to just `object`

--- Nim/lib/mod3.nim
+++ Nim/lib/mod3.nim
@@ -1,12 +1,12 @@
 type
-  Person* = ref object
+  Person* = object
     name: string
     age: int16
 
-  Point* = ref object of RootObj
+  Point* = object of RootObj
     # Note that fields also needs to be accesible if
     # u don't have a constructor for this object
     x*, y*: float
 
-  Rect* = ref object of Point
+  Rect* = object of Point
     w*, h*: int
