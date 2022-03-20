6694ad208d5c9267714066f41dd20e0cb398d1b1
Miqueas <miqueas2020@yahoo.com>
2022-03-20 14:02:16 +0000

    Nim: minor changes in reading files

--- Nim/reading_files.nim
+++ Nim/reading_files.nim
@@ -1,9 +1,9 @@
 import strutils
 
-var file = "test_file.txt".readfile()
+var file = readFile("test_file.txt")
 
-echo """Contents of "test_file.txt":"""
+echo "Contents of 'test_file.txt':"
 echo file
-echo file.splitlines()
+echo file.splitLines()
 # 'strip' removes empty lines at the end
-echo file.strip().splitlines()
+echo file.strip().splitLines()
