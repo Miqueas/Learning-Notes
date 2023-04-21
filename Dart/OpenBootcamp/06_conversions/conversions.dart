daa0473a4c894ab0778fc8c9aa7c1a9fb531e614
Miqueas <miqueas2020@yahoo.com>
2023-04-20 20:03:21 -0500

    Dart: minor changes in conversions

--- Dart/OpenBootcamp/06_conversions/conversions.dart
+++ Dart/OpenBootcamp/06_conversions/conversions.dart
@@ -1,4 +1,7 @@
 void main(List<String> args) {
   String oneHundredString = 100.toString();
   int oneHundredInteger = int.parse(oneHundredString);
+
+  print(oneHundredString);
+  print(oneHundredInteger);
 }
