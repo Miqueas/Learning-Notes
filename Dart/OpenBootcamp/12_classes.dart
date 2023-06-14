43edcd3e41de5953822a39eecf6c75d65e5e0235
Miqueas <miqueas2020@yahoo.com>
2023-06-14 10:56:28 -0500

    Dart: added classes

---
+++ Dart/OpenBootcamp/12_classes.dart
@@ -0,0 +1,31 @@
+class Person {
+  String name;
+  int _age;
+
+  Person(this.name, this._age);
+
+  int get age => this._age;
+
+  set age(int age) {
+    if (age > 0) {
+      this._age = age;
+    } else {
+      throw Error();
+    }
+  }
+
+  void greet() {
+    print("Hi! My name is ${this.name} and I'm ${this._age}yo!");
+  }
+
+  void say(String message) {
+    print("${this.name}: $message");
+  }
+}
+
+void main(List<String> args) {
+  final me = Person("Miqueas", 22);
+  me.greet();
+  me.say("hey!");
+  me.say("wanna try Dart?");
+}
