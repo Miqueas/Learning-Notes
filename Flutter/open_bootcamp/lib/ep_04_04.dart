0696ca9d0b43a42e881cf8fce8d283f655968d0b
Miqueas <miqueas2020@yahoo.com>
2024-01-16 07:06:33 -0500

    Flutter: OpenBootcamp: agregado ep_04_04.dart

---
+++ Flutter/open_bootcamp/lib/ep_04_04.dart
@@ -0,0 +1,89 @@
+import "package:flutter/material.dart";
+
+const appTitle = "Flutter OpenBootcamp";
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title}) : super();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Icon likedIcon = const Icon(Icons.favorite_border);
+  bool liked = false;
+
+  void _doLike() {
+    setState(() {
+      liked = !liked;
+      likedIcon = Icon((liked) ? Icons.favorite : Icons.favorite_border);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(onPressed: _doLike, icon: likedIcon)
+        ],
+      ),
+      drawer: const Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text("Menu")),
+            Text("Item 1"),
+            Text("Item 2"),
+            Text("Item 3"),
+            Text("Item 4"),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "New",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox),
+            label: "Inbox",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Account",
+          ),
+        ],
+      ),
+    );
+  }
+}
