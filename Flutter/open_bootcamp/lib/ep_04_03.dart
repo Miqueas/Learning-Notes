0b084721c78080fda3be2e62c20bf46697764713
Miqueas <miqueas2020@yahoo.com>
2024-01-16 07:05:42 -0500

    Flutter: OpenBootcamp: renombrados archivos (de nuevo...)

--- Flutter/open_bootcamp/lib/ep_04_03.dart
+++ Flutter/open_bootcamp/lib/ep_04_03.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   final String title;
 
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title}) : super();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
