1583250e16a647dfb45a00345794e445c7ece334
Miqueas <miqueas2020@yahoo.com>
2024-01-19 22:35:40 -0500

    Flutter: OpenBootcamp: agregado ep_04_07

---
+++ Flutter/open_bootcamp/lib/ep_04_07.dart
@@ -0,0 +1,59 @@
+import "package:flutter/material.dart";
+
+const appTitle = "Flutter OpenBootcamp";
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle, style: TextStyle(fontWeight: FontWeight.bold)),
+          actions: [
+            const Text("IconButton"),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.phone_android)),
+          ],
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(onPressed: () {}, child: const Text("Elevated"))
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: FilledButton(onPressed: () {}, child: const Text("Filled"))
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: FilledButton.tonal(onPressed: () {}, child: const Text("Filled Tonal"))
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: OutlinedButton(onPressed: () {}, child: const Text("Outlined"))
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: TextButton(onPressed: () {}, child: const Text("Text"))
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: const Text("Floating Action Button"),
+        ),
+      ),
+    );
+  }
+}
