3448c244d305959f6ea20488baf79170d2e8732a
Miqueas <miqueas2020@yahoo.com>
2024-01-28 12:18:25 -0500

    Flutter: OpenBootcamp: cambios menores en los archivos de windows

--- Flutter/open_bootcamp/windows/runner/main.cpp
+++ Flutter/open_bootcamp/windows/runner/main.cpp
@@ -25,9 +25,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"open_bootcamp", origin, size)) {
+  Win32Window::Point origin(20, 20);
+  Win32Window::Size size(400, 400);
+  if (!window.Create(L"OpenBootcamp", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
