c76f578126fe5fde9e2afc10bb2302c15094cc71
Miqueas <miqueas2020@yahoo.com>
2024-01-29 10:33:38 -0500

    Flutter: OpenBootcamp: terminado ep_04_14.dart

--- Flutter/OpenBootcamp/lib/ep_04_14.dart
+++ Flutter/OpenBootcamp/lib/ep_04_14.dart
@@ -33,6 +33,34 @@ class MyApp extends StatelessWidget {
               ),
               child: const Text("C"),
             ),
+            Container(
+              height: 60,
+              width: 120,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                  style: BorderStyle.solid,
+                )
+              ),
+              child: const Text("D"),
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.green,
+              alignment: Alignment.bottomRight,
+              child: const Text("E"),
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.indigo,
+              alignment: const Alignment(0.4, -0.4),
+              child: const Text("E"),
+            ),
           ],
         ),
       ),
