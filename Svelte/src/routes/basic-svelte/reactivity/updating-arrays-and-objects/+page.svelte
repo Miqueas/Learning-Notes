a6a79a421681d54c9d627be9a97aae1007d96842
Miqueas <miqueas2020@yahoo.com>
2024-01-31 05:44:00 -0500

    Svelte: added updating arrays and objects

---
+++ Svelte/src/routes/basic-svelte/reactivity/updating-arrays-and-objects/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let numbers = [1, 2, 3, 4]
+  $: sum = numbers.reduce((total, current) => total + current, 0)
+
+  function doAddNumber() {
+    // Using `.push()` doesn't trigger reactivity since Svelte's reactivity system works by assignments.
+    numbers = [...numbers, numbers.length + 1]
+  }
+</script>
+
+<main>
+  <p>{numbers.join(" + ")} = {sum}</p>
+  <button on:click={doAddNumber}>Add number</button>
+</main>
