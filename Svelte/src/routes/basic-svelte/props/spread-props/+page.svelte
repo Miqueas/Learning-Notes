1d1c37ce4c836522c1ed8b7ff62f874a14931e67
Miqueas <miqueas2020@yahoo.com>
2024-02-01 07:47:56 -0500

    Svelte: added "spread props"

---
+++ Svelte/src/routes/basic-svelte/props/spread-props/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import PackageInfo from "./PackageInfo.svelte";
+
+  const pkg = {
+    name: "svelte",
+    speed: "blazing",
+    version: 4,
+    website: "https://svelte.dev",
+  }
+</script>
+
+<main>
+  <PackageInfo {...pkg}/>
+</main>
---
+++ Svelte/src/routes/basic-svelte/props/spread-props/PackageInfo.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name
+  export let version
+  export let speed
+  export let website
+
+  $: href = `https://www.npmjs.com/package/${name}`
+</script>
+
+<main>
+  <p>
+    The <code>{name}</code> package is {speed} fast. Download version {version} from
+    <a {href}>npm</a> and learn more <a href={website}>here</a>.
+  </p>
+</main>
