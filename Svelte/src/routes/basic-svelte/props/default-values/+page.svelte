136ad2aad89ca5d10c45ee4be0779ac63f67215e
Miqueas <miqueas2020@yahoo.com>
2024-02-01 07:47:39 -0500

    Svelte: added "default values"

---
+++ Svelte/src/routes/basic-svelte/props/default-values/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Nested from "./Nested.svelte";
+</script>
+
+<main>
+  <Nested answer={42}/>
+  <Nested/>
+</main>
---
+++ Svelte/src/routes/basic-svelte/props/default-values/Nested.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let answer = "a mistery";
+</script>
+
+<main>
+  <p>The answer is {answer}</p>
+  <p>Type of `answer`: {typeof(answer)}</p>
+</main>
