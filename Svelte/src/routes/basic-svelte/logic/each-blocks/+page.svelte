76737f71f3a3992cc14314bcec5e5e0d11ca9db4
Miqueas <miqueas2020@yahoo.com>
2024-02-02 20:27:03 -0500

    Svelte: added "each blocks"

---
+++ Svelte/src/routes/basic-svelte/logic/each-blocks/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  let colors = [ "#db0f3b", "#f0501e", "#dbab0f", "#3bdb0f", "#0f60db", "#820fdb" ];
+  let selected = colors[0];
+</script>
+
+<main>
+  <div class="box" style="background-color: {selected}"></div>
+  <h1>Pick a color</h1>
+  {#each colors as color, index}
+    <button
+      aria-current={selected == color}
+      aria-label={color}
+      style="background-color: {color}"
+      on:click={() => selected = color}
+    >{index + 1}</button>
+  {/each}
+</main>
+
+<style>
+  .box {
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+  }
+
+  button {
+    margin: 4px;
+    padding: 14px;
+  }
+
+  button[aria-current="true"] {
+    padding: 18px;
+    font-weight: bold;
+    font-size: 14pt;
+  }
+</style>
