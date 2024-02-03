13062e0dff13aa6e632b7f0d7b41058e9fa5e820
Miqueas <miqueas2020@yahoo.com>
2024-02-02 20:25:44 -0500

    Svelte: added "if blocks"

---
+++ Svelte/src/routes/basic-svelte/logic/if-blocks/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  let count = 0
+
+  function doIncrement() {
+    ++count
+  }
+</script>
+
+<main>
+  <p>Clicked {count} {count === 1 ? "time" : "times"}</p>
+  <button on:click={doIncrement}>Click me!</button>
+  {#if count > 10}
+    <p>You clicked more than 10 times!</p>
+  {/if}
+</main>
