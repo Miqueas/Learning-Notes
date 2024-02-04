96e02fd4b72e17d17990460225034dd6dcbd4c45
Miqueas <miqueas2020@yahoo.com>
2024-02-04 17:54:02 -0500

    Svelte: added "dom events"

---
+++ Svelte/src/routes/basic-svelte/events/dom-events/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  let mouse = { x: 0, y: 0 }
+
+  function doHandleMouseMove(event) {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+  }
+</script>
+
+<main>
+  <div on:pointermove={doHandleMouseMove}>
+    Mouse pointer is at {mouse.x} x {mouse.y}
+  </div>
+</main>
+
+<style>
+  div {
+    color: #fafafa;
+    font-weight: bold;
+    padding: 100px;
+    height: 600px;
+    background-color: rgb(240, 80, 30);
+  }
+</style>
