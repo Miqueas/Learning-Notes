d1e7db7ffc6300aea41ac860613da4d1774c073a
Miqueas <miqueas2020@yahoo.com>
2024-02-04 17:55:04 -0500

    Svelte: added "component events"

---
+++ Svelte/src/routes/basic-svelte/events/component-events/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import MyEvent from "./MyEvent.svelte"
+
+  function doHandleEvent(event) {
+    alert(event.detail.text)
+  }
+</script>
+
+<main>
+  <MyEvent on:greet={doHandleEvent}/>
+</main>
---
+++ Svelte/src/routes/basic-svelte/events/component-events/MyEvent.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from "svelte"
+
+  const dispatch = createEventDispatcher()
+
+  function doDispatchEvent() {
+    dispatch("greet", { text: "Hi there!" })
+  }
+</script>
+
+<main>
+  <button on:click={doDispatchEvent}>Click me!</button>
+</main>
