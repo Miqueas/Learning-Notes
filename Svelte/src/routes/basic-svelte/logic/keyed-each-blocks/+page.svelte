5fba71ae8d85abd0c4b81e52258e47f78838c55e
Miqueas <miqueas2020@yahoo.com>
2024-02-03 22:38:26 -0500

    Svelte: added "keyed each blocks"

---
+++ Svelte/src/routes/basic-svelte/logic/keyed-each-blocks/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Emoji from "./Emoji.svelte"
+
+  let name = ""
+  let list = [
+    { id: 0, name: "apple" },
+    { id: 1, name: "banana" },
+    { id: 2, name: "carrot" },
+    { id: 3, name: "egg" },
+    { id: 4, name: "doughnut" },
+    { id: 5, name: "eggplant" },
+    { id: 6, name: "potato" },
+  ]
+
+  function doRemove() {
+    list = list.slice(1)
+  }
+</script>
+
+<main>
+  <h1>Some emojis</h1>
+  <button on:click={doRemove}>Remove first</button>
+  <ul>
+    {#each list as item (item.id)}
+      <li><Emoji name={item.name}></Emoji></li>
+    {/each}
+  </ul>
+</main>
---
+++ Svelte/src/routes/basic-svelte/logic/keyed-each-blocks/Emoji.svelte
@@ -0,0 +1,9 @@
+<script>
+  import * as emoji from "node-emoji"
+
+  export let name
+</script>
+
+<main>
+  <p>{emoji.get(name)} = {name}</p>
+</main>
