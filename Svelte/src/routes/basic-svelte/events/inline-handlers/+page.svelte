c8c002978255caf533716265671ca2a98ba9bcc0
Miqueas <miqueas2020@yahoo.com>
2024-02-04 17:54:19 -0500

    Svelte: added "inline handlers"

---
+++ Svelte/src/routes/basic-svelte/events/inline-handlers/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  let mouse = { x: 0, y: 0 }
+</script>
+
+<main>
+  <div on:pointermove={
+    (event) => mouse = { x: event.clientX, y: event.clientY }
+  }>
+    Mouse pointer is at {mouse.x} x {mouse.y}
+  </div>
+</main>
+
+<style>
+  div {
+    color: #fafafa;
+    font-weight: bold;
+    padding: 100px;
+    height: 600px;
+    background-color: rgb(240, 80, 30);
+  }
+</style>
