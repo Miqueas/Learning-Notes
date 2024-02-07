389faaa571bf5df465b5fe9aa912f3f7683fde6a
Miqueas <miqueas2020@yahoo.com>
2024-02-06 21:22:19 -0500

    Svelte: added "dom event forwarding"

---
+++ Svelte/src/routes/basic-svelte/events/dom-event-forwarding/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import MyButton from "./MyButton.svelte"
+  import horn from "./horn.mp3"
+
+  const audio = new Audio()
+  audio.src = horn
+
+  function doPlay() {
+    audio.load()
+    audio.play()
+  }
+</script>
+
+<main>
+  <MyButton on:click={doPlay}/>
+</main>
---
+++ Svelte/src/routes/basic-svelte/events/dom-event-forwarding/MyButton.svelte
@@ -0,0 +1,9 @@
+<button on:click>Push!</button>
+
+<style>
+  button {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+</style>
