3ea7d5c130db6add5c93fd823109fdfe218fdf47
Miqueas <miqueas2020@yahoo.com>
2024-02-08 20:37:54 -0500

    Svelte: added "numeric inputs"

---
+++ Svelte/src/routes/basic-svelte/bindings/numeric-inputs/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  let x = 0
+  let y = 0
+</script>
+
+<main>
+  <p><strong>X</strong>({x}) + <strong>Y</strong>({y}) = {x + y}</p>
+
+  <label>
+    <span><strong>X</strong> = </span>
+    <input type="number" bind:value={x} min="0" max="100" />
+    <input type="range" bind:value={x} min="0" max="100" />
+  </label>
+  <br/>
+  <label>
+    <span><strong>Y</strong> = </span>
+    <input type="number" bind:value={y} min="0" max="100" />
+    <input type="range" bind:value={y} min="0" max="100" />
+  </label>
+</main>
