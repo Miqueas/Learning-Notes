c8e1cdf483833d168a63239528badf198fecc68a
Miqueas <miqueas2020@yahoo.com>
2024-02-08 20:38:24 -0500

    Svelte: added "checkbox inputs"

---
+++ Svelte/src/routes/basic-svelte/bindings/checkbox-inputs/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  let accepted = false
+  let display = "block"
+
+  function doCloseMessage() {
+    display = "none"
+  }
+</script>
+
+<main>
+  <div style="display: {display};">
+    <p>This site has cookies, do you accept them?</p>
+    <input type="checkbox" bind:checked={accepted} />
+    <span>Yes</span>
+    <br/><br/>
+    <button disabled={!accepted} on:click={doCloseMessage}>Close message</button>
+  </div>
+</main>
