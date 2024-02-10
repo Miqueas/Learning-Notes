cf5aabbdb4bd92d413444ce1a6cec96c2b035188
Miqueas <miqueas2020@yahoo.com>
2024-02-09 21:28:03 -0500

    Svelte: added "select bindings"

---
+++ Svelte/src/routes/basic-svelte/bindings/select-bindings/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  let selected
+  let answer = ""
+  let questions = [
+    { id: 1, text: "Where did you go to school?"},
+    { id: 2, text: "What is your favorite color?"},
+    { id: 3, text: "What is your favorite animal?"},
+    { id: 4, text: "What is your favorite food?"},
+    { id: 5, text: "What is your mother's name?"},
+    { id: 6, text: "What is your father's name?"},
+  ]
+
+  function doHandleSubmit() {}
+</script>
+
+<main>
+  <h1>Security questions</h1>
+
+  <form on:submit|preventDefault={doHandleSubmit}>
+    <select bind:value={selected} on:change={() => { answer = "" }}>
+      {#each questions as question}
+        <option value={question.id}>{question.text}</option>
+      {/each}
+    </select>
+
+    <input bind:value={answer} />
+
+    <button disabled={!answer} type="submit">Submit</button>
+  </form>
+
+  <p>Selected question: {selected ? selected : "(none yet)"}</p>
+</main>
