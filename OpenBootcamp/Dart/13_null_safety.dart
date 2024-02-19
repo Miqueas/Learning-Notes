c327f6e4da1c7b3d133782515d13e5cbede27b45
Miqueas <miqueas2020@yahoo.com>
2024-02-18 20:03:01 -0500

    OpenBootcamp: Dart: cambios menores

--- OpenBootcamp/Dart/13_null_safety.dart
+++ OpenBootcamp/Dart/13_null_safety.dart
@@ -1,26 +1,28 @@
 void main() {
+  // NOTA: descomentalas líneas necesarias para ver los errores
+
   // Non-nullable
   int x;
   // Nullable
   int? y;
   // Non-nullable, will be initialized later
-  late int z;
+  // late int z;
 
   // Error: `x` isn't initialized yet
-  print(x);
+  // print(x);
   // Ok, `y` is nullable
   print(y);
   // Same as `x`, don't have a value yet
-  print(z);
+  // print(z);
 
   x = 29;
   // Error: `y` is nullable and is `null` here, because isn't initialized yet
-  z = x + y;
+  // z = x + y;
 
   // Ok
   print(x);
   // Sometimes you may need to use the "null assertion operator" (!), which
   // will throw an error during runtime if the operands is `null`
   print(y!);
-  print(z);
+  // print(z);
 }
