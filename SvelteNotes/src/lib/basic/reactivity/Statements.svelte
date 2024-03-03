c0866b0adce381eb65b51c6b9eab226076cc1a5a
Miqueas <miqueas2020@yahoo.com>
2024-03-03 17:51:50 -0500

    Svelte: moved "reactivity"

---
+++ SvelteNotes/src/lib/basic/reactivity/Statements.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0
+
+  // `$` accepts almost anything, including code blocks
+  $: if (count >= 10) {
+    alert(`Count (${count}) is too high!`)
+    count = 0
+  }
+
+  function doIncrementCount() { ++count }
+</script>
+
+<main>
+  <p>Clicked {count} {count === 1 ? "time" : "times"}!</p>
+  <button on:click={doIncrementCount}>Increment</button>
+</main>
