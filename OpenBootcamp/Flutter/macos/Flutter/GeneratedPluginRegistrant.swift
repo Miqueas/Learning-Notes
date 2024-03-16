24c283cdba5b5548a0a781873303d75fcf1e08b3
Miqueas <miqueas2020@yahoo.com>
2024-03-15 21:46:54 -0500

    OpenBootcamp: Flutter: finalizado ep_10_01.dart

--- OpenBootcamp/Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ OpenBootcamp/Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import file_selector_macos
 import location
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
