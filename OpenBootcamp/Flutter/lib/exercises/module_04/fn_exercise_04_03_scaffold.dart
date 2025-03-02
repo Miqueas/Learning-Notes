2a82e04393c4fe9e8c56a3f5bfd020b286f9e67b
Josué Martínez <JosueTech@proton.me>
2025-03-01 20:24:12 -0500

    OpenBootcamp: Flutter: ejercicio 3, módulo 4

---
+++ OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_03_scaffold.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+
+final class FnExercise0403Scaffold extends StatelessWidget {
+  const FnExercise0403Scaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Title"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.favorite_border_rounded),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          icon: const Icon(Icons.add),
+          label: const Text("New"),
+          onPressed: () {},
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Account",
+          ),
+        ],),
+      ),
+    );
+  }
+}
