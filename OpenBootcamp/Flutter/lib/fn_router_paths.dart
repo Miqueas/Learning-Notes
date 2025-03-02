7dcc603926f3f5545e82db724a2e65cfca49bf5d
Josué Martínez <JosueTech@proton.me>
2025-03-01 19:51:52 -0500

    OpenBootcamp: Flutter: creado FnRouterPaths

---
+++ OpenBootcamp/Flutter/lib/fn_router_paths.dart
@@ -0,0 +1,65 @@
+final class FnRouterPaths {
+  static const home = '/';
+
+  // Module 04
+  static const module04 = '/module04';
+  static const module04Exercise01 = '/module04/exercise01';
+  static const module04Exercise02 = '/module04/exercise02';
+  static const module04Exercise03 = '/module04/exercise03';
+  static const module04Exercise04 = '/module04/exercise04';
+  static const module04Exercise05 = '/module04/exercise05';
+  static const module04Exercise06 = '/module04/exercise06';
+  static const module04Exercise07 = '/module04/exercise07';
+  static const module04Exercise08 = '/module04/exercise08';
+  static const module04Exercise09 = '/module04/exercise09';
+  static const module04Exercise10 = '/module04/exercise10';
+  static const module04Exercise11 = '/module04/exercise11';
+  static const module04Exercise12 = '/module04/exercise12';
+  static const module04Exercise13 = '/module04/exercise13';
+  static const module04Exercise14 = '/module04/exercise14';
+  static const module04Exercise15 = '/module04/exercise15';
+
+  // Module 05
+  static const module05 = '/module05';
+  static const module05Exercise01 = '/module05/exercise01';
+  static const module05Exercise02 = '/module05/exercise02';
+  static const module05Exercise03 = '/module05/exercise03';
+
+  // Module 06
+  static const module06 = '/module06';
+  static const module06Exercise01 = '/module06/exercise01';
+  static const module06Exercise02 = '/module06/exercise02';
+  static const module06Exercise03 = '/module06/exercise03';
+  static const module06Exercise04 = '/module06/exercise04';
+  static const module06Exercise05 = '/module06/exercise05';
+
+  // Module 07
+  static const module07 = '/module07';
+  static const module07Exercise01 = '/module07/exercise01';
+  static const module07Exercise02 = '/module07/exercise02';
+  static const module07Exercise03 = '/module07/exercise03';
+  static const module07Exercise04 = '/module07/exercise04';
+  static const module07Exercise05 = '/module07/exercise05';
+  static const module07Exercise06 = '/module07/exercise06';
+  static const module07Exercise07 = '/module07/exercise07';
+  static const module07Exercise08 = '/module07/exercise08';
+  static const module07Exercise09 = '/module07/exercise09';
+  static const module07Exercise10 = '/module07/exercise10';
+
+  // Module 08
+  static const module08 = '/module08';
+  static const module08Exercise01 = '/module08/exercise01';
+  static const module08Exercise02 = '/module08/exercise02';
+  static const module08Exercise03 = '/module08/exercise03';
+  static const module08Exercise04 = '/module08/exercise04';
+
+  // Module 09
+  static const module09 = '/module09';
+  static const module09Exercise01 = '/module09/exercise01';
+  static const module09Exercise02 = '/module09/exercise02';
+
+  // Module 10
+  static const module10 = '/module10';
+  static const module10Exercise01 = '/module10/exercise01';
+  static const module10Exercise02 = '/module10/exercise02';
+}
