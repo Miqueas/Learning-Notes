86985bf04cb0cb7aaaa7884f55beb4ad9b0d50ec
Josué Martínez <JosueTech@proton.me>
2025-03-01 20:27:51 -0500

    OpenBootcamp: Flutter: cambios en el router:
    - La propiedad modules ahora es público y estático
    - Eliminada propiedad routes (no tenía mucho sentido)
    - El método _buildRoutes ahora es público y estático
    - El método _buildScreen se movió fuera de la clase (para que _buildRoutes pueda ser estático)

--- OpenBootcamp/Flutter/lib/fn_router.dart
+++ OpenBootcamp/Flutter/lib/fn_router.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/exercises/module_04/fn_exercise_04_01_hello.dart';
 import 'package:flutter_notes/exercises/module_04/fn_exercise_04_02_stateful.dart';
+import 'package:flutter_notes/exercises/module_04/fn_exercise_04_03_scaffold.dart';
 import 'package:flutter_notes/fn_router_paths.dart';
 import 'package:flutter_notes/types/fn_exercise.dart';
 import 'package:flutter_notes/types/fn_module.dart';
 import 'package:flutter_notes/widgets/fn_list_item.dart';
 import 'package:sprintf/sprintf.dart';
 
+Widget _buildScreen(FnModule module, [ FnExercise? exercise ]) => Material(
+  child: Scaffold(
+    appBar: AppBar(
+      toolbarHeight: 72,
+      title: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(exercise?.topic ?? module.topic,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          sprintf(
+            switch (exercise != null) {
+              true => 'Exercise %02d - Module %02d',
+              false => 'Module %02d'
+            },
+            [ if (exercise != null) exercise.id, module.id, ],
+          ),
+          style: const TextStyle(fontSize: 14,),
+        ),
+      ],),
+    ),
+    body: exercise?.content ?? ListView(children: [
+      for (final exercise in module.exercises) FnListItem(
+        title: exercise.topic,
+        subtitle: sprintf('Exercise %02d', [ exercise.id ]),
+        route: exercise.route,
+      ),
+    ],),
+  ),
+);
+
 final class FnRouter {
-  final _modules = const {
+  static const modules = {
     FnModule(
       id: 1,
       topic: 'Getting started',
@@ -26,16 +60,32 @@ final class FnRouter {
           route: FnRouterPaths.module04Exercise02,
           content: FnExercise0402Stateful(),
         ),
-      }
+        FnExercise(
+          id: 3,
+          topic: 'Scaffold',
+          route: FnRouterPaths.module04Exercise03,
+          content: FnExercise0403Scaffold(),
+        ),
+        FnExercise(
+          id: 4,
+          topic: 'Drawer',
+          route: FnRouterPaths.module04Exercise04,
+          content: FnExercise0402Stateful(),
+        ),
+        FnExercise(
+          id: 5,
+          topic: 'Cupertino',
+          route: FnRouterPaths.module04Exercise05,
+          content: FnExercise0402Stateful(),
+        ),
+      },
     ),
   };
 
-  Map<String, WidgetBuilder> get routes => _buildRoutes();
-
-  Map<String, WidgetBuilder> _buildRoutes() {
+  static Map<String, WidgetBuilder> buildRoutes() {
     final routes = <String, WidgetBuilder>{};
 
-    for (final module in _modules) {
+    for (final module in modules) {
       routes[module.route] = (context) => _buildScreen(module);
       for (final exercise in module.exercises) {
         routes[exercise.route] = (context) => _buildScreen(module, exercise);
@@ -44,39 +94,4 @@ final class FnRouter {
 
     return routes;
   }
-
-  Widget _buildScreen(FnModule module, [ FnExercise? exercise ]) => Material(
-    child: Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(exercise?.topic ?? module.topic,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              sprintf(
-                switch (exercise != null) {
-                  true => 'Exercise %02d - Module %02d',
-                  false => 'Module %02d'
-                },
-                [ if (exercise != null) exercise.id, module.id, ],
-              ),
-              style: const TextStyle(fontSize: 14,),
-            ),
-          ],
-        ),
-      ),
-      body: exercise?.content ?? ListView(children: [
-        for (final exercise in module.exercises) FnListItem(
-          title: exercise.topic,
-          subtitle: sprintf('Exercise %02d', [ exercise.id ]),
-          route: exercise.route,
-        ),
-      ],),
-    ),
-  );
 }
