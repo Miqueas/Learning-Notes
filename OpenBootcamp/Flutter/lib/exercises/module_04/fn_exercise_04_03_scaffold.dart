9dd4bb5d0ecc96f6430c2616ae048cbcb13ad039
Josué Martínez <JosueTech@proton.me>
2025-03-01 21:55:20 -0500

    OpenBootcamp: Flutter: cambios menores:
    - Ahora el drawer se abre inmediatamente en el ejercicio 4, ésto con la intención de que se haga notar mejor

--- OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_03_scaffold.dart
+++ OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_03_scaffold.dart
@@ -4,39 +4,37 @@ final class FnExercise0403Scaffold extends StatelessWidget {
   const FnExercise0403Scaffold({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Title"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.favorite_border_rounded),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          icon: const Icon(Icons.add),
-          label: const Text("New"),
-          onPressed: () {},
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Title"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_border_rounded),
+            onPressed: () {},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: "Explore",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "Account",
-          ),
-        ],),
+        ],
       ),
-    );
-  }
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        label: const Text("New"),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.explore),
+          label: "Explore",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: "Account",
+        ),
+      ],),
+    ),
+  );
 }
--- OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_04_drawer.dart
+++ OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_04_drawer.dart
@@ -1,51 +1,66 @@
 import "package:flutter/material.dart";
+import "package:flutter/scheduler.dart";
 
-final class FnExercise0404Drawer extends StatelessWidget {
+final class FnExercise0404Drawer extends StatefulWidget {
   const FnExercise0404Drawer({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Title"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.favorite_border_rounded),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        drawer: const Drawer(child: Column(children: [
-          DrawerHeader(child: Text("Menu"),),
-          Text("Item 1"),
-          Text("Item 2"),
-          Text("Item 3"),
-          Text("Item 4"),
-          Text("Item 5"),
-          Text("Item 6"),
-        ],),),
-        floatingActionButton: FloatingActionButton.extended(
-          icon: const Icon(Icons.add),
-          label: const Text("New"),
-          onPressed: () {},
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: "Explore",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "Account",
-          ),
-        ],),
-      ),
-    );
+  State<FnExercise0404Drawer> createState() => _FnExercise0404DrawerState();
+}
+
+final class _FnExercise0404DrawerState extends State<FnExercise0404Drawer> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _scaffoldKey.currentState?.openDrawer();
+    });
   }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text("Title"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_border_rounded),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: const Drawer(child: Column(children: [
+        DrawerHeader(child: Text("Menu"),),
+        Text("Item 1"),
+        Text("Item 2"),
+        Text("Item 3"),
+        Text("Item 4"),
+        Text("Item 5"),
+        Text("Item 6"),
+      ],),),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        label: const Text("New"),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.explore),
+          label: "Explore",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: "Account",
+        ),
+      ],),
+    ),
+  );
 }
