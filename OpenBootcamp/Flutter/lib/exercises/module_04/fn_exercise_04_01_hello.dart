17c3b6adb65622bea3741d758c25a0124ec3ee30
Josué Martínez <JosueTech@proton.me>
2025-03-01 19:48:06 -0500

    OpenBootcamp: Flutter: cambios diversos:
    - Eliminado routes.dart
    - Renombrado ep_04_01_hello.dart a fn_exercise_04_01_hello.dart
    - Eliminado module_04.dart
    - Renombrada carpeta routes a exercises

---
+++ OpenBootcamp/Flutter/lib/exercises/module_04/fn_exercise_04_01_hello.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final class FnExercise0401 extends StatelessWidget {
+  const FnExercise0401({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Center(
+    child: Text('Hello World!')
+  );
+}
