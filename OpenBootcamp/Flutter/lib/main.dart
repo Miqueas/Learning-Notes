0aca4afe7c0b2e855ebb4d78be260f76eae9746c
Josué Martínez <JosueTech@proton.me>
2025-03-01 20:29:28 -0500

    OpenBootcamp: Flutter: cambios menores en main:
    - Comentados todos los FnListItem para ahora usar FnRouter.modules
    - Ahora se usa FnRouter.buildRoutes()

--- OpenBootcamp/Flutter/lib/main.dart
+++ OpenBootcamp/Flutter/lib/main.dart
@@ -1,9 +1,9 @@
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_notes/fn_router.dart";
-import "package:flutter_notes/fn_router_paths.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:flutter_notes/widgets/fn_list_item.dart";
+import "package:sprintf/sprintf.dart";
 
 void main() => runApp(const FnApp());
 
@@ -37,7 +37,7 @@ final class FnApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
     title: "Flutter Notes",
     theme: _buildTheme(Brightness.light),
-    routes: FnRouter().routes,
+    routes: FnRouter.buildRoutes(),
     darkTheme: _buildTheme(Brightness.dark),
     debugShowCheckedModeBanner: kDebugMode,
     home: Material(child: Scaffold(
@@ -51,42 +51,47 @@ final class FnApp extends StatelessWidget {
           ),
         ],),
       ),
-      body: ListView(children: const [
-        FnListItem(
-          title: "Getting started",
-          subtitle: "Module 04",
-          route: FnRouterPaths.module04,
+      body: ListView(children: [
+        for (final module in FnRouter.modules) FnListItem(
+          title: module.topic,
+          subtitle: sprintf('Module %02d', [ module.id ]),
+          route: module.route,
         ),
-        FnListItem(
-          title: "Color and logic",
-          subtitle: "Module 05",
-          route: FnRouterPaths.module05,
-        ),
-        FnListItem(
-          title: "Displaying information",
-          subtitle: "Module 06",
-          route: FnRouterPaths.module06,
-        ),
-        FnListItem(
-          title: "User inputs",
-          subtitle: "Module 07",
-          route: FnRouterPaths.module07,
-        ),
-        FnListItem(
-          title: "Dynamic content and Inkwell",
-          subtitle: "Module 08",
-          route: FnRouterPaths.module08,
-        ),
-        FnListItem(
-          title: "Location and image picker",
-          subtitle: "Module 09",
-          route: FnRouterPaths.module09,
-        ),
-        FnListItem(
-          title: "Networking",
-          subtitle: "Module 10",
-          route: FnRouterPaths.module10,
-        ),
+        // FnListItem(
+        //   title: "Getting started",
+        //   subtitle: "Module 04",
+        //   route: FnRouterPaths.module04,
+        // ),
+        // FnListItem(
+        //   title: "Color and logic",
+        //   subtitle: "Module 05",
+        //   route: FnRouterPaths.module05,
+        // ),
+        // FnListItem(
+        //   title: "Displaying information",
+        //   subtitle: "Module 06",
+        //   route: FnRouterPaths.module06,
+        // ),
+        // FnListItem(
+        //   title: "User inputs",
+        //   subtitle: "Module 07",
+        //   route: FnRouterPaths.module07,
+        // ),
+        // FnListItem(
+        //   title: "Dynamic content and Inkwell",
+        //   subtitle: "Module 08",
+        //   route: FnRouterPaths.module08,
+        // ),
+        // FnListItem(
+        //   title: "Location and image picker",
+        //   subtitle: "Module 09",
+        //   route: FnRouterPaths.module09,
+        // ),
+        // FnListItem(
+        //   title: "Networking",
+        //   subtitle: "Module 10",
+        //   route: FnRouterPaths.module10,
+        // ),
       ],),
     ),),
   );
