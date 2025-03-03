874652634871129431e5b50cc9acb6b1d4a31bc5
Josué Martínez <JosueTech@proton.me>
2025-03-02 21:09:06 -0500

    OpenBootcamp: Flutter: agregado botón para cambio de tema

--- OpenBootcamp/Flutter/lib/main.dart
+++ OpenBootcamp/Flutter/lib/main.dart
@@ -1,14 +1,23 @@
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_notes/fn_router.dart";
+import "package:flutter_notes/fn_router_paths.dart";
 import "package:format/format.dart";
+import "package:gap/gap.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:flutter_notes/widgets/fn_list_item.dart";
 
 void main() => runApp(const FnApp());
 
-final class FnApp extends StatelessWidget {
+final class FnApp extends StatefulWidget {
   const FnApp({ super.key });
+
+  @override
+  State<FnApp> createState() => _FnAppState();
+}
+
+final class _FnAppState extends State<FnApp> {
+  var _themeMode = ThemeMode.system;
 
   ThemeData _buildTheme(Brightness mode) {
     final baseTheme = ThemeData.from(
@@ -31,21 +40,39 @@ final class FnApp extends StatelessWidget {
     title: "Flutter Notes",
     theme: _buildTheme(Brightness.light),
     routes: FnRouter.buildRoutes(),
+    themeMode: _themeMode,
     darkTheme: _buildTheme(Brightness.dark),
     debugShowCheckedModeBanner: kDebugMode,
     home: Material(child: Scaffold(
       appBar: AppBar(
-        toolbarHeight: 96,
-        centerTitle: true,
-        title: const Column(mainAxisSize: MainAxisSize.min, children: [
-          Text("Flutter Notes", style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),),
-          Text("A small app I made while learning Flutter",
-            style: TextStyle(fontSize: 14,),
+        title: const Text("Flutter Notes", style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),),
+        actions: [
+          IconButton(
+            icon: Icon(switch (_themeMode) {
+              ThemeMode.system => Icons.brightness_medium_rounded,
+              ThemeMode.light => Icons.light_mode_rounded,
+              ThemeMode.dark => Icons.dark_mode_rounded,
+            }),
+            onPressed: () => setState(() {
+              _themeMode = switch (_themeMode) {
+                ThemeMode.system => ThemeMode.light,
+                ThemeMode.light => ThemeMode.dark,
+                ThemeMode.dark => ThemeMode.system,
+              };
+            }),
           ),
-        ],),
+          const Gap(8),
+          Builder(builder: (context) => IconButton(
+            icon: const Icon(Icons.info_rounded),
+            onPressed: () => Navigator.of(context).pushNamed(
+              FnRouterPaths.about
+            ),
+          ),),
+          const Gap(12),
+        ],
       ),
       body: ListView(children: [
         for (final module in FnRouter.modules) FnListItem(
