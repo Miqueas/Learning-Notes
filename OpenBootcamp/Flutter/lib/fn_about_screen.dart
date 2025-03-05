cb637fd493dd983a8b4f04b41f21c22c75d21e2a
Josué Martínez <JosueTech@proton.me>
2025-03-04 22:18:24 -0500

    OpenBootcamp: Flutter: cambios menores en about screen

--- OpenBootcamp/Flutter/lib/fn_about_screen.dart
+++ OpenBootcamp/Flutter/lib/fn_about_screen.dart
@@ -11,60 +11,55 @@ final class FnAboutScreen extends StatelessWidget {
       fontSize: 24,
       fontWeight: FontWeight.bold,
     ),),),
-    body: ListView(padding: const EdgeInsets.all(16), children: [
-      SizedBox(
-        height: 240,
-        child: Center(child: Column(mainAxisSize: MainAxisSize.min, children: [
-          const Text('Flutter Notes', style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w900,
-          ),),
-          const Text('A simple app made while I was learning Flutter',),
-          const SizedBox(
-            width: 200,
-            child: Divider(thickness: 1, height: 24,),
-          ),
-          Row(mainAxisSize: MainAxisSize.min, children: [
-            const Text('By',),
-            const Gap(8),
-            GestureDetector(
-              onTap: () async {
-                final ok = await showDialog<bool>(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Open Github profile?',),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: const Text('Cancel'),
-                      ),
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(true),
-                        child: const Text('Open'),
-                      ),
-                    ],
-                  ),
-                );
-
-                if (ok == true) {
-                  launchUrl(Uri.parse('https://github.com/Miqueas'));
-                }
-              },
-              child: Row(mainAxisSize: MainAxisSize.min, children: [
-                const CircleAvatar(
-                  radius: 12,
-                  foregroundImage: AssetImage('assets/images/me.jpg'),
-                ),
-                const Gap(8),
-                Text('Josué Martínez', style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),),
-              ],),
-            ),
-          ],)
-        ],),),
+    body: Center(child: Column(mainAxisSize: MainAxisSize.min, children: [
+      const Text('Flutter Notes', style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w900,
+      ),),
+      const Text('A simple app made while I was learning Flutter',),
+      const SizedBox(
+        width: 200,
+        child: Divider(thickness: 1, height: 24,),
       ),
-    ],),
+      Row(mainAxisSize: MainAxisSize.min, children: [
+        const Text('By',),
+        const Gap(8),
+        GestureDetector(
+          onTap: () async {
+            final ok = await showDialog<bool>(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text('Open Github profile?',),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text('Open'),
+                  ),
+                ],
+              ),
+            );
+    
+            if (ok == true) {
+              launchUrl(Uri.parse('https://github.com/Miqueas'));
+            }
+          },
+          child: Row(mainAxisSize: MainAxisSize.min, children: [
+            const CircleAvatar(
+              radius: 12,
+              foregroundImage: AssetImage('assets/images/me.jpg'),
+            ),
+            const Gap(8),
+            Text('Josué Martínez', style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+          ],),
+        ),
+      ],)
+    ],),),
   ),);
 }
