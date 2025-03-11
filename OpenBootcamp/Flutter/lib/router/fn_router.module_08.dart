cc3e7a2e9b7d2bf93760b36909a33c71d544bf6f
Josué Martínez <JosueTech@proton.me>
2025-03-11 01:03:09 -0500

    OpenBootcamp: Flutter: actualizadas rutas

--- OpenBootcamp/Flutter/lib/router/fn_router.module_08.dart
+++ OpenBootcamp/Flutter/lib/router/fn_router.module_08.dart
@@ -4,5 +4,18 @@ const _module08 = FnModule(
   id: 8,
   topic: "Dynamic content and Inkwell",
   route: FnPaths.module08,
-  exercises: {}
+  exercises: {
+    FnExercise(
+      id: 1,
+      topic: 'ListView',
+      route: FnPaths.module08Exercise01,
+      content: FnExercise0801ListView(),
+    ),
+    FnExercise(
+      id: 2,
+      topic: 'Dissmisible',
+      route: FnPaths.module08Exercise02,
+      content: FnExercise0802Dissmisible(),
+    ),
+  }
 );
