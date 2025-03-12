974b27ef292b331b6f085e373f7739d80f6cf690
Josué Martínez <JosueTech@proton.me>
2025-03-11 23:04:42 -0500

    OpenBootcamp: Flutter: finalizado módulo 9

--- OpenBootcamp/Flutter/lib/router/fn_router.module_09.dart
+++ OpenBootcamp/Flutter/lib/router/fn_router.module_09.dart
@@ -4,5 +4,18 @@ const _module09 = FnModule(
   id: 9,
   topic: "Location and image picker",
   route: FnPaths.module09,
-  exercises: {}
+  exercises: {
+    FnExercise(
+      id: 1,
+      topic: 'ImagePicker',
+      route: FnPaths.module09Exercise01,
+      content: FnExercise0901ImagePicker(),
+    ),
+    FnExercise(
+      id: 2,
+      topic: 'Location',
+      route: FnPaths.module09Exercise02,
+      content: FnExercise0902Location(),
+    ),
+  }
 );
